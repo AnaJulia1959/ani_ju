b8edb935690778adfe791adcb01aa6405096727b
AnaJulia1959 <anajuliarodrigues1959@gmail.com>
2025-06-09 21:03:41 -0300

    android erro

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.ani_ju"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
